9484d964659bebefb0cb63533996c6031776ec2d
varun-r-boop <varunpanthom6555@gmail.com>
2024-06-18 15:07:30 +0530

    fixed module not found issue | 1.0.8

--- dynamic-tooltip-library/projects/dynamic-tooltip/src/lib/dynamic-tooltip.directive.ts
+++ dynamic-tooltip-library/projects/dynamic-tooltip/src/lib/dynamic-tooltip.directive.ts
@@ -5,15 +5,16 @@ import { TooltipCondition } from './tooltip-condition.interface';
   selector: '[appDynamicTooltip]'
 })
 export class DynamicTooltipDirective implements OnInit {
-  @Input() tooltipData: any;
-  @Input() tooltipConditions: TooltipCondition[] = [];
+  @Input('appDynamicTooltip') tooltipConfig: { data: any, conditions: TooltipCondition[] } = { data: null, conditions: [] };
+
   constructor(private el: ElementRef, private renderer: Renderer2) { }
 
   ngOnInit() {
-    if (this.tooltipData && this.tooltipConditions) {
-      for (const condition of this.tooltipConditions) {
-        if (condition.condition(this.tooltipData)) {
-          const message = typeof condition.message === 'function' ? condition.message(this.tooltipData) : condition.message;
+    const { data, conditions } = this.tooltipConfig;
+    if (data && conditions) {
+      for (const condition of conditions) {
+        if (condition.condition(data)) {
+          const message = typeof condition.message === 'function' ? condition.message(data) : condition.message;
           this.setTooltip(message);
           break;
         }
